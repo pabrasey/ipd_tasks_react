1fa9ae5550bd6537a9df53dbaf9a27de42087ab1
pabrasey <pabrasey@gmail.com>
2019-12-17 16:28:56 +0100

    comment on deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,5 @@ var TaskList = artifacts.require("./TaskList.sol");
 module.exports = async function(deployer) {
   deployer.deploy(TaskList);
   tasklist = await TaskList.deployed();
-  await tasklist.createTask('A new task', "description");
+  await tasklist.createTask('A new task', "description"); // DOESNT WORK !
 };
