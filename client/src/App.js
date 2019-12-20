f2e424bbed944feefc5dcbfdd056dbbe1a544f39
pabrasey <pabrasey@gmail.com>
2019-12-20 14:55:28 +0100

    first try with drizzle-react doesn't work

--- client/src/App.js
+++ client/src/App.js
@@ -1,113 +1,32 @@
-import React, { Component } from "react";
-import TaskListContract from "./contracts/TaskList.json";
-import PPCTokenContract from "./contracts/PPCToken.json";
-import getWeb3 from "./getWeb3";
-import TaskTable from "./taskTable";
-import UpdateTask from "./updateTask";
-import CreateTask from "./createTask";
-import BalanceTable from "./balanceTable"
+import React from "react";
+import IpdApp from './ipdApp';
+import { DrizzleContext } from "drizzle-react";
 
-import "./App.css";
+const App = () => (
 
-class App extends Component {
-  state = { web3: null, accounts: null, contract: null, task_count: 0, tasks: [], ppc_balances: [] };
+//DrizzleContext Consumer gives us access to our drizzle objects
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-      accounts.push("0x299818F98284FC7dbE0721827A5678FD091B91A2", "0x1AEf5FCA1cd59978214b70bd734334F3f93F88be", 
-      "0xebd7F835f3E3c80ab1F2F3F05dd40398EeA4d1F7");
-      const networkId = await web3.eth.net.getId();
-      console.log('accounts: ', accounts);
-
-      // Get the TaskList instance.
-      const TaskListdeployedNetwork = TaskListContract.networks[networkId];
-      const tasklist = new web3.eth.Contract(
-        TaskListContract.abi,
-        TaskListdeployedNetwork && TaskListdeployedNetwork.address,
-      );
-
-      // Get the PPCToken instance.
-      const PPCTokendeployedNetwork = PPCTokenContract.networks[networkId];
-      const ppctoken = new web3.eth.Contract(
-        PPCTokenContract.abi,
-        PPCTokendeployedNetwork && PPCTokendeployedNetwork.address,
-      );
-
-      console.log('ppctoken instance: ', ppctoken);
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, tasklist, ppctoken }, this.getContractData);
-
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+    <DrizzleContext.Consumer>
+    {
+        drizzleContext => 
+        {
+        const { drizzle, 
+                drizzleState, 
+                initialized } = drizzleContext;
+        //return a loading UI if drizzle is not yet initialised
+        if(!initialized){
+            return "Loading...";
+        }
+        
+        //pass drizzle down as props into a subcomponent
+        return (
+        <IpdApp 
+            drizzle={drizzle} 
+            drizzleState={drizzleState} 
+        />)
+        }
     }
-
-  };
-
-  getContractData = async () => {
-
-    const { accounts, tasklist } = this.state;
-
-    // TaskList
-
-    const task_count = await tasklist.methods.task_count().call();
-    const tasks = [];
-
-    for (var i = 0; i < task_count; i++) {
-      const task = await tasklist.methods.tasks(i).call();
-      const validators = await tasklist.methods.getValidators(i).call();
-      task.validators = validators;
-      tasks.push(task);
-    }
-
-    console.log('tasks: ', tasks);
-
-    // PPCToken
-
-    //this.state.ppctoken.methods.mint(accounts[3],(1).toString()).send({ from: accounts[0] })
-
-    let ppc_balances = [];
-    for(var i = 0; i < accounts.length; i++){
-      let balance = await this.state.ppctoken.methods.balanceOf(accounts[i]).call();
-      let el = {account: accounts[i], balance: balance};
-      ppc_balances.push(el);
-    };
-    console.log('ppc_balances: ', ppc_balances);
-
-    this.setState({ task_count, tasks, ppc_balances });
-
-  }
-
-  render() {
-
-    const tasks = this.state.tasks;
-
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div>
-        <div className="App">
-          <h1>IPD Task Manager</h1>
-          <p>The number of tasks is: { this.state.task_count }</p>
-        </div>
-        <TaskTable tasks = { tasks } />
-        <UpdateTask web3 = {this.state.web3} contract = {this.state.tasklist} tasks = { tasks } />
-        <CreateTask web3 = {this.state.web3} contract = {this.state.tasklist} />
-        <BalanceTable balances = { this.state.ppc_balances } />
-      </div>
-    );
-  }
-
-}
+    </DrizzleContext.Consumer>
+)
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Drizzle, generateStore } from "drizzle";
+import { DrizzleContext } from "../node_modules/drizzle-react";
+import drizzleOptions from "./drizzleOptions";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const drizzleStore = generateStore(drizzleOptions);
+const drizzle = new Drizzle(drizzleOptions, drizzleStore);
+
+console.log('version: ', React.version);
+
+ReactDOM.render(
+
+    <DrizzleContext.Provider drizzle={drizzle}>
+        <App />
+    </DrizzleContext.Provider>, 
+
+    document.getElementById('root')
+    
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
